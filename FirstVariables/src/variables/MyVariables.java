a6d1c7b0d6b10b4f9f1652d845e6e4be9f5d40a0
parkerguy8 <60110819+parkerguy8@users.noreply.github.com>
2020-01-20 14:48:49 -0500

    Add files via upload

---
+++ FirstVariables/src/variables/MyVariables.java
@@ -0,0 +1,21 @@
+package variables;
+
+public class MyVariables {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int myInteger = 34;
+		double myDouble= 65.8;
+		boolean myBoolean = true;
+		char Aliens;
+		System.out.println("myInteger: " + myInteger);
+		System.out.println("myDouble: " + myDouble);
+		System.out.println("myBoolean: " + myBoolean);
+		System.out.println("Aliens are time travelers = " + myBoolean);
+
+
+
+	}
+
+}
+
