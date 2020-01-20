f156807110bce26a5b306082e439b5532d2b1ee8
parkerguy8 <60110819+parkerguy8@users.noreply.github.com>
2020-01-20 14:58:20 -0500

    Add files via upload

--- FirstVariables/src/variables/MyVariables.java
+++ FirstVariables/src/variables/MyVariables.java
@@ -4,14 +4,14 @@ public class MyVariables {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int myInteger = 34;
+		int myInteger = 42;
 		double myDouble= 65.8;
 		boolean myBoolean = true;
 		char Aliens;
-		System.out.println("myInteger: " + myInteger);
-		System.out.println("myDouble: " + myDouble);
-		System.out.println("myBoolean: " + myBoolean);
-		System.out.println("Aliens are time travelers = " + myBoolean);
+		System.out.println("Whats the Meaning of Life?: " + myInteger);
+		System.out.println("Give a random Double value: " + myDouble);
+		System.out.println("Give a value for Boolean: " + myBoolean);
+		System.out.println("Are Aliens are time travelers?: " + myBoolean);
 
 
 
